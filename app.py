6f9bacb4e38d9f5cbbb3eeadd9acf5f712476cb0
S0kku <sofiakan2006@gmail.com>
2026-05-03 03:32:22 +0300

    Store categories in database

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ def show_item(item_id):
 @app.route("/new_item")
 def new_item():
     require_login()
-    return render_template("new_item.html")
+    classes = (items.get_all_classes())
+    return render_template("new_item.html", classes=classes)
 
 @app.route("/create_item", methods=["POST"])
 def create_item():
@@ -62,12 +63,10 @@ def create_item():
     user_id = session["user_id"]
 
     classes = []
-    section = request.form["section"]
-    if section:
-        classes.append(("Main", section))
-    cost = request.form["cost"]
-    if cost:
-        classes.append(("Cost", cost))
+    for entry in request.form.getlist("classes"):
+        if entry:
+            parts = entry.split(":")
+            classes.append((parts[0], parts[1]))
 
     items.add_item(title, description, user_id, classes)
 
---
+++ init.sql
@@ -0,0 +1,12 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Main', 'Active / Outdoors');
+INSERT INTO classes (title, value) VALUES ('Main', 'Arts / Crafts');
+INSERT INTO classes (title, value) VALUES ('Main', 'Food / Restaurants');
+INSERT INTO classes (title, value) VALUES ('Main', 'Games / Series');
+INSERT INTO classes (title, value) VALUES ('Main', 'Museum / Sightseeing');
+
+INSERT INTO classes (title, value) VALUES ('Cost', 'High');
+INSERT INTO classes (title, value) VALUES ('Cost', 'Medium');
+INSERT INTO classes (title, value) VALUES ('Cost', 'Low');
+INSERT INTO classes (title, value) VALUES ('Cost', 'Free');
